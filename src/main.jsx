2a51b63cca95fa839e3fd720c153c2b1251671ec
sibashdb <sebastiandb2@icloud.com>
2026-05-18 17:23:33 -0700

    Corregir extension de importacion de store

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 // Nota: asegúrate de que tu archivo se llame store.js o store.jsx según lo hayas creado
-import { store } from './store/store.jsx'; 
+import { store } from './Store/store.jsx'; 
 
 // Agrega esto para pintar el fondo de todo el sitio web
 document.body.style.backgroundColor = '#0b0813'; // Un morado ultra oscuro y elegante
